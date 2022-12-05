fc8d81da95138c42fbc083b31791b835db217bca
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-12-05 22:40:02 +0400

    В процессе выполнения слайдер карусель

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-import sliderСarousel from './modules/sliderСarousel';
+import {sliderСarousel} from './modules/sliderСarousel';
 sliderСarousel();
--- src/modules/sliderСarousel.js
+++ src/modules/sliderСarousel.js
@@ -1,5 +1,31 @@
 "use strict";
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+
+
 const sliderСarousel = () => {
-    console.log("hey");
+
+    const swiper = new Swiper('.companies-swiper', {
+        modules: [Navigation, Pagination],
+        slidesPerView: 3,
+        navigation: {
+            nextEl: ".benefits__arrow--right",
+            prevEl: ".benefits__arrow--left",
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+        },
+
+    });
 };
-export default sliderСarousel;
+
+
+export {sliderСarousel};
