95d882d6edb67bac77e874c2fb23639e6131d73b
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-12-06 11:45:00 +0400

    Подключил плавный склол

---
+++ src/modules/smoothScroll.js
@@ -0,0 +1,27 @@
+"use strict";
+const smoothScroll = () => {
+    const goTopBtn = document.querySelector('.smooth-scroll');
+
+    // Скролл по стрелке
+    goTopBtn.style.display = 'none';
+    function trackScroll() {
+        const offSet = window.pageYOffset;
+
+        if (offSet > 620) {
+            goTopBtn.style.display = 'block';
+        } else {
+            goTopBtn.style.display = 'none';
+        }
+    }
+
+    function goTop() {
+        if (window.pageYOffset > 0) {
+            window.scrollBy(0, -30);
+            setTimeout(goTop, 0);
+        }
+    }
+
+    goTopBtn.addEventListener('click', goTop);
+    window.addEventListener('scroll', trackScroll);
+};
+export default smoothScroll;
