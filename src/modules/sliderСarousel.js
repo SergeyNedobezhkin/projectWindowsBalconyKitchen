60bc497233488b1a9156394e25f085d564ef1e65
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-12-23 19:45:36 +0400

    Реализовал слайдер карусель

--- src/modules/sliderСarousel.js
+++ src/modules/sliderСarousel.js
@@ -1,31 +1,20 @@
 "use strict";
 
-import Swiper, { Navigation, Pagination } from 'swiper';
-
-
-
+import Swiper, { Navigation, Pagination } from "swiper";
 const sliderСarousel = () => {
-
-    const swiper = new Swiper('.companies-swiper', {
-        modules: [Navigation, Pagination],
+  const swiper = new Swiper(".swiper", {
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    navigation: {
+      nextEl: ".benefits__arrow--right",
+      prevEl: ".benefits__arrow--left",
+    },
+    breakpoints: {
+      576: {
         slidesPerView: 3,
-        navigation: {
-            nextEl: ".benefits__arrow--right",
-            prevEl: ".benefits__arrow--left",
-        },
-        breakpoints: {
-            640: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 40,
-            },
-        },
-
-    });
+        spaceBetween: 40,
+      },
+    },
+  });
 };
-
-
-export {sliderСarousel};
+export { sliderСarousel };
