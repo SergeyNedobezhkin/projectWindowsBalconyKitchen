a8cf9b9754b2699ccdd0258ccb2baa17fe124199
Sergey Nedobezhkin <zelenka1633@gmail.com>
2023-01-15 19:19:18 +0400

    Подключил слайдер

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -4,13 +4,13 @@ import Swiper, { Navigation, Pagination } from "swiper";
 const slider = () => {
   const swiperSlider = new Swiper(".swiperSlider", {
     modules: [Navigation, Pagination],
-    slidesPerView: 2,
+    slidesPerView: 1,
     navigation: {
       nextEl: ".services__arrow--right",
       prevEl: ".services__arrow--left",
     },
     breakpoints: {
-      576: {
+      577: {
         slidesPerView: 2,
         spaceBetween: 40,
       },
