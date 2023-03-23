597dad1012867202fd3d7bd81635ef16ac519583
Sergey Nedobezhkin <zelenka1633@gmail.com>
2023-03-23 12:43:16 +0400

    Реализовал увеличение изображений сертификатов

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import modalСall from "./modules/modalWindows";
 import calc from "./modules/calc";
 import check from "./modules/check";
 import phoneInput from "./modules/phoneInput";
-
 import sendForm from "./modules/sendForm";
+
 timer("10 dec 2023");
-smoothScroll();
-slider();
 sliderСarousel();
+slider();
+smoothScroll();
 modalСall();
 calc();
 check();
